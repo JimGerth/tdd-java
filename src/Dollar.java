540cdeba2f6a255ccef53dbdef41c27f68419c64
JimGerth <jim.frederic.gerth@gmail.com>
2019-08-01 11:16:49 +0200

    -- page 6 -- first test passing

---
+++ src/Dollar.java
@@ -0,0 +1,15 @@
+
+public class Dollar {
+
+	public int amount = 10;
+
+	public Dollar(int amount) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void times(int multiplier) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
